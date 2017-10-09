df8851976e674e0dc2c59d2e67205a85afbef365
Pfaff, Carey (US - Arlington) <cpfaff@deloitte.com>
2017-10-09 17:06:40 -0400

    Updated home and added keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Keyboard } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -33,6 +34,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.configureKeyboard();
     });
   }
 
@@ -40,5 +42,58 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  private configureKeyboard() {
+    Keyboard.hideKeyboardAccessoryBar(false);
+
+    if (this.platform.is('ios')) {
+      let keyBoardOpen : boolean = false; // Will be true when keyboard is open
+      let offsetY; // Tapped Y co-ordinate from bottom
+      Keyboard.disableScroll(true);
+      let paddingAdded = 0;
+      window.addEventListener('native.keyboardshow', function(e){
+        
+        let nearestContentTop = 0;
+        let inputTop = 0;
+        // Get the active element's y co-ordinate.
+        inputTop = document.activeElement.parentElement.getBoundingClientRect().top+document.activeElement.parentElement.getBoundingClientRect().height;
+        // AS the keyboard is already shown, the total height = innderHeight + keyboardHeight
+        if (inputTop > window.innerHeight) {
+          offsetY = (window.innerHeight + e['keyboardHeight']) - inputTop;
+        } else {
+          offsetY = (window.innerHeight + e['keyboardHeight']) - inputTop + 100;
+        }
+        let scrollEl = document.activeElement.closest('.scroll-content');
+        if(scrollEl) {
+          nearestContentTop = parseInt(scrollEl['style'].marginTop.slice(0, -2));
+        }
+
+        let keyboardHeight = e['keyboardHeight'];
+
+        let bodyMove = document.querySelector("ion-app");
+        let bodyMoveStyle = bodyMove['style'];
+        if (offsetY < keyboardHeight + 40) {
+          bodyMoveStyle.bottom = (paddingAdded ? paddingAdded + keyboardHeight - offsetY : keyboardHeight - offsetY + 40) + "px";
+          paddingAdded = paddingAdded ? paddingAdded + keyboardHeight - offsetY : keyboardHeight - offsetY + 40;
+          bodyMoveStyle.top = "initial";
+        }
+        keyBoardOpen = true;
+      });
+
+      window.addEventListener('native.keyboardhide', function(e){
+        paddingAdded = 0;
+        let removeStyles = document.querySelector("ion-app");
+        removeStyles.removeAttribute("style");
+        keyBoardOpen = false;
+      });
+
+      // On touch, collect the Y co-ordinate so that the bottom of the body can be padded when keyboard opens up.
+      window.addEventListener('touchstart', function(e){
+        let clientYPosition = e['touches'][0].clientY;
+        let height = window.innerHeight;
+        offsetY = (height - clientYPosition);
+      });
+    }
   }
 }
